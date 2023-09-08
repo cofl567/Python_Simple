46a4dd6492ea85831dd036af9d19eee42447714e
cofl567 <133830206+cofl567@users.noreply.github.com>
2023-09-08 14:47:44 +0900

    Add python_01_variable

---
+++ lecture/python_01_variable.py
@@ -0,0 +1,10 @@
+# 주석(comment): 메모, 실행X
+
+# 1. 출력 함수(print)
+# - ()안의 값을 출력
+# - 변수값 확인용도로 많이 사용
+print("Hello Python")
+
+# 문자열 타입(String Type)
+# - Python: '' or "" -> string
+# - C, Java: ''(char), ""(string)
