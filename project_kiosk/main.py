3bfc88ad385501b3ea1e2155e868871e1fa854e7
cofl567 <133830206+cofl567@users.noreply.github.com>
2023-10-18 16:37:16 +0900

    Add files via upload

--- project_kiosk/main.py
+++ project_kiosk/main.py
@@ -24,43 +24,68 @@ menu_save = []  # 고객 주문 메뉴 기록
 price_save = [] # 고객 주문 가격 기록
 
 # 1. 메인 메뉴 출력
-print("■" * 50)
-print("■■ == 조선별다방 == ")
-print("■■ == ver 1.2")
-print("■■ 메인 메뉴")
-for i, menu in enumerate(main_name.values()):
-    print(f"■□  {i+1}.{menu}")
+while True:
+    print("■" * 50)
+    print("■■ == 조선별다방 == ")
+    print("■■ == ver 1.2")
+    print("■■ 메인 메뉴")
+    for i, menu in enumerate(main_name.values()):
+        print(f"■□  {i+1}.{menu}")
 
-# 2.메인 메뉴 선택
-choice = user_choice(len(main_name), "main")
+    # 2.메인 메뉴 선택
+    choice = user_choice(len(main_name), "main")
 
-# 3.서브 메뉴 출력
-if choice == 1: # 커피
-    print("●● 커피(Coffee)")
-    for i in range(len(coffee_name)):
-        print(f"●○  {i+1}.{coffee_name[i+1]}({coffee_price[i+1]}원)")
-    choice = user_choice(len(coffee_name))
-    menu_save.append(coffee_name[choice])
-    price_save.append(coffee_price[choice])
-elif choice == 2: # 음료
-    print("oo 음료(drink)")
-    for key, value in drink_name.items():
-        print(f"oo  {key}.{value}({drink_price[key]}원)")
-    choice = user_choice(len(drink_name))
-    menu_save.append(drink_name[choice])
-    price_save.append(drink_price[choice])
-elif choice == 3: # 빵
-    print("oo 빵(bakery)")
-    for i, value in enumerate(bakery_name.values()):
-        print(f"oo  {i+1}.{value}({bakery_price[i+1]}원)")
-    choice = user_choice(len(bakery_name))
-    menu_save.append(bakery_name[choice])
-    price_save.append(bakery_price[choice])
-elif choice == 99:
-    print("MSG: 조선별다방 키오스크를 종료합니다.")
-    exit()
+    # 3.서브 메뉴 출력
+    if choice == 1: # 커피
+        print("●● 커피(Coffee)")
+        for i in range(len(coffee_name)):
+            print(f"●○  {i+1}.{coffee_name[i+1]}({coffee_price[i+1]}원)")
+    # 4.서브 메뉴 선택
+        choice = user_choice(len(coffee_name))
+    # 5.선택 메뉴 주문 목록 저장
+        menu_save.append(coffee_name[choice])
+        price_save.append(coffee_price[choice])
+    elif choice == 2: # 음료
+        print("oo 음료(drink)")
+        for key, value in drink_name.items():
+            print(f"oo  {key}.{value}({drink_price[key]}원)")
+        choice = user_choice(len(drink_name))
+        menu_save.append(drink_name[choice])
+        price_save.append(drink_price[choice])
+    elif choice == 3: # 빵
+        print("oo 빵(bakery)")
+        for i, value in enumerate(bakery_name.values()):
+            print(f"oo  {i+1}.{value}({bakery_price[i+1]}원)")
+        choice = user_choice(len(bakery_name))
+        menu_save.append(bakery_name[choice])
+        price_save.append(bakery_price[choice])
+    elif choice == 99:
+        print("MSG: 조선별다방 키오스크를 종료합니다.")
+        exit()
 
-for menu in menu_save:
-    print(menu)
-for price in price_save:
-    print(price)
+    # 6. 추가 주문 yes or no?
+    print("●○ 추가 주문하시겠습니까?(y/n)")
+    flog = 0 # 추가 주문 y/n 여부
+    while True:
+        choice_yn = input("y/n: ")
+        if choice_yn == "y" or choice_yn == "Y":
+            break
+        elif choice_yn.lower() == "n":
+            flag =1
+            break
+        else:
+            print("MSG: y 또는 n만 입력하세요.")
+
+# 7.주문내역 출력!
+    if flag == 1:
+        print("="*50)
+        print("== 고객님이 주문하신 메뉴 ==")
+        for i, menu in enumerate(menu_save):
+            print(f"==  {i+1}.{menu}")
+
+        total_price = 0 # 총 결제금액
+        for price in price_save:
+            total_price += price
+
+        print(f"MSG: 주문하신 메뉴는 {len(menu_save)}개로 총 결제금액은 {total_price}원 입니다.")
+        print("MSG: 이용해주셔서 감사합니다.")
