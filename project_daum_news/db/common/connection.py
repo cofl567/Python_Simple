2ef115046a83e093add1fe3a126304e96baab7c1
cofl567 <133830206+cofl567@users.noreply.github.com>
2023-11-08 17:33:59 +0900

    Add files via upload

---
+++ project_daum_news/db/common/connection.py
@@ -0,0 +1,28 @@
+# 파일시스템: 폴더 또는 디렉토리로 데이터를 저장하는 방법
+# 데이터베이스: 데이터를 효율적으로 저장하고 관리해주는 시스템(이론)
+
+# 데이터베이스 관리 시스템(DBMS): 데이터베이스 제품!
+
+# ** DBMS 종류
+# 1.관계형 데이터베이스(RDB) : 전통적인(스키마:명세서)
+#   - ORACLE
+#   - MySQL
+#   - MariaDB
+
+# 2.NoSQL : New(빅데이터)
+#   - MongoDB
+#   - Redis
+
+# ** DMBS 프로세스
+#   - DB에 ID와PW 저장
+#   Pycharm(Python) ----------------------------------------- DB(MongoDB)
+#   1.Pycharm과 DB Connection 맺기
+#    - IP: 내 컴퓨터를 접속 할 수 있는 주소
+#    - PORT: 컴퓨터 내에 프로그램의 위치
+#    - ID and PW
+#   2.SQL을 사용해서 작업 진행
+#    - SQL(구조질의어): RDB를 사용하기 위해서는 반드시 사용!
+#   CREATE      -> INSERT
+#   READ        -> SELEVT
+#   UPDATE      -> UPDATE
+#   DELETE      -> DELETE
